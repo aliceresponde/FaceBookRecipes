38f7e6f01841f444600a869bfb07ab9ec4a53f25
Alicia Beltran <aliceresponde@gmail.com>
2016-07-03 11:14:11 -0500

    Confit  loginBtn for faceBook

--- app/src/main/java/com/example/alice/facebookrecipes/db/entities/Recipe.java
+++ app/src/main/java/com/example/alice/facebookrecipes/db/entities/Recipe.java
@@ -5,6 +5,9 @@ import com.google.gson.annotations.SerializedName;
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.PrimaryKey;
 import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import java.sql.DatabaseMetaData;
 
 /**
  * Created by alice on 7/3/16.
@@ -12,7 +15,7 @@ import com.raizlabs.android.dbflow.annotation.Table;
  */
 
 @Table(database = RecipiesDataBase.class)
-public class Recipe {
+public class Recipe extends BaseModel {
     @SerializedName("recipe_id")
     @PrimaryKey private String  recipeID;
 
--- app/src/main/java/com/example/alice/facebookrecipes/login/ui/LoginActivity.java
+++ app/src/main/java/com/example/alice/facebookrecipes/login/ui/LoginActivity.java
@@ -1,15 +1,94 @@
 package com.example.alice.facebookrecipes.login.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.LauncherApps;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.SupportActionModeWrapper;
+import android.widget.RelativeLayout;
 
 import com.example.alice.facebookrecipes.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnLogin)
+    LoginButton btnLogin;
+    @BindView(R.id.container)
+    RelativeLayout container;
+
+    private CallbackManager  callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        if (AccessToken.getCurrentAccessToken() != null){
+            navigateToMainScreen();
+        }
+
+
+
+        callbackManager = CallbackManager.Factory.create();
+        btnLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                      navigateToMainScreen();
+            }
+
+            @Override
+            public void onCancel() {
+                Snackbar.make(container, R.string.login_cancel_error , Snackbar.LENGTH_SHORT ).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                String strError = String.format(getString(R.string.login_error) , error.getLocalizedMessage());
+                Snackbar.make(container, strError , Snackbar.LENGTH_SHORT ).show();
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //restult of FB intent
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void navigateToMainScreen() {
+        Intent intent = new Intent( this, RecipeMainActivity.class);
+        intent.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP
+                     |   Intent.FLAG_ACTIVITY_NEW_TASK
+                     |   Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 }
